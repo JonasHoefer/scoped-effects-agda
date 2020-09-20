ce7041b8b9780e22e0c35f609d528b1cc06fbf22
jhoefer <jonas.hoefer99@gmail.com>
2020-09-20 11:59:43 +0200

    Update test scripts

--- tool/check_all.sh
+++ tool/check_all.sh
@@ -7,11 +7,14 @@ root_dir=$(dirname "$script_dir")
 cd "$root_dir"
 
 echo "module Everything where" > Everything.agda
-for f in $(find . -name "*.agda")
+for d in src example
 do
-  if [ "$f" = "./Everything.agda" ]; then
-    continue
-  fi
-  echo "import$( grep -m 1 -Po "(?<=module)\s+[a-zA-Z\.0-9]+\s+(?=where)" $f )" >> Everything.agda
+    for f in $(find $d -name "*.agda")
+    do
+    if [ "$f" = "./Everything.agda" ]; then
+        continue
+    fi
+    echo "import$( grep -m 1 -Po "(?<=module)\s+[a-zA-Z\.0-9]+\s+(?=where)" $f )" >> Everything.agda
+    done
 done
 agda --verbose=2 -i "." Everything.agda
