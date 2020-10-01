046397e43ecc0d7b23cf701df1f1c125fbeb8934
jhoefer <jonas.hoefer99@gmail.com>
2020-10-01 19:07:29 +0200

    Add more text

--- thesis/make.sh
+++ thesis/make.sh
@@ -2,6 +2,8 @@
 set -e
 agda --latex thesis.lagda.tex
 agda --latex FirstOrder.lagda.tex
+agda --latex HigherOrder.lagda.tex
+agda --latex ScopedAlgebras.lagda.tex
 cd latex
 lualatex -shell-escape thesis.tex
 biber thesis
